194228b7de52d95fc4b12b609d87e9183705555f
JoshAshby <joshuaashby@joshashby.com>
2016-06-21 16:37:35 -0600

    cache in dokku? and some more info in telegram replies

--- config/initializers/004_cache.rb
+++ config/initializers/004_cache.rb
@@ -1,5 +1,5 @@
 class MrMarkov
   def self.cache
-    @cache ||= ActiveSupport::Cache.lookup_store :redis_store, AshFrame.config_for(:redis).symbolize_keys.merge({ driver: :hiredis })
+    @cache ||= ActiveSupport::Cache.lookup_store :redis_store, **AshFrame.config_for(:redis).symbolize_keys.merge({ driver: :hiredis })
   end
 end
