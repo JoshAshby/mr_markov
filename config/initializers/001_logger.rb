bbaba01d9c6a9860ed14a99575bd22f6ab929402
JoshAshby <joshuaashby@joshashby.com>
2016-06-27 15:48:17 -0600

    try sync file

--- config/initializers/001_logger.rb
+++ config/initializers/001_logger.rb
@@ -8,7 +8,7 @@ end
 
 class MrMarkov
   def self.logger
-    @logger ||= Logger.new(AshFrame.root.join('logs', 'mr_markov.log').open('a')).tap do |logger|
+    @logger ||= Logger.new(AshFrame.root.join('logs', 'mr_markov.log').open('a').tap{ |f| f.sync = true }).tap do |logger|
       # setup our logger for everything...
       logger.level = Logger::DEBUG if AshFrame.environment == :development
       logger.extend ActiveSupport::Logger.broadcast(Logger.new(STDOUT)) if AshFrame.environment != :test
