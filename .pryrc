8dc9f1cdc22877181acfaee86becc41f00afb78a
JoshAshby <joshuaashby@joshashby.com>
2016-10-10 17:16:20 -0600

    add pryrc and make flow_graph read in things

---
+++ .pryrc
@@ -0,0 +1,3 @@
+require './mr_markov'
+
+Util.read_dir Dir.new( AshFrame.root.join('nodes', 'http_request') )
--- lib/flow_graph.rb
+++ lib/flow_graph.rb
@@ -1,15 +1,5 @@
-class FlowGraph
+class Util
   class << self
-    def injest_config config
-      binding.pry
-    end
-
-    attr_accessor :logger
-
-    def logger
-      @_logger ||= Logger.new
-    end
-
     def read_dir dir
       root = Pathname.new dir
 
@@ -23,6 +13,60 @@ class FlowGraph
       end
     end
   end
+end
+
+class FlowGraph
+  class << self
+    def build config
+      nodes = {}
+      edges = {}
+
+      nodes = config['nodes'].each_with_object({}) do |node_config, memo|
+        unbound_block = Nodes.registry[ node_config['type'] ]
+
+        node = Node.new node_config['id'], node_config['name']
+        node.instance_exec node_config['properties'], &unbound_block
+
+        memo[ node_config['id'] ] = node
+      end
+
+      edges = config.fetch('edges', []).each_with_object({}) do |edge_config, memo|
+        memo[ edge_config['from_node_id'] ] ||= []
+        memo[ edge_config['from_node_id'] ] << edge_config['to_node_id']
+      end
+
+      graph = {
+        id: config['id'],
+        name: config['name'],
+        nodes: nodes,
+        edges: edges
+      }
+
+      binding.pry
+    end
+  end
+
+  class Node
+    def initialize id, name
+      @id   = id
+      @name = name
+    end
+
+    def on event_key, &block
+      events[ event_key.to_sym ] ||= []
+      events[ event_key.to_sym ] << block
+
+      nil
+    end
+
+    def emit event_key, *payload
+      events[ event_key.to_sym ].map{ |e| e.call(*payload) }
+    end
+
+    def events
+      @events ||= {}
+    end
+  end
 
   class Nodes
     class << self
@@ -33,82 +77,8 @@ class FlowGraph
       end
 
       def register node_type, &block
-        registry[ node_type ] = NodesDSL.new.cloak(&block).events
+        registry[ node_type ] = block
       end
     end
   end
-
-  class NodesDSL
-    def on event_key, &block
-      events[ event_key.to_sym ] ||= []
-      events[ event_key.to_sym ] << block
-
-      nil
-    end
-
-    def events
-      @events ||= {}
-    end
-
-    # Stolen from https://github.com/robacarp/andrew/blob/6c297d3a98d39ff7a4060efa9a4a1c24b70b3153/lib/andrew/web/proxy.rb#L67
-    # Which is in turn adapted from: http://www.skorks.com/2013/03/a-closure-is-not-always-a-closure-in-ruby/
-    def cloak *args, closure: nil, &b
-      executor = self.class.class_eval do
-        define_method :dsl_executor, &b
-        meth = instance_method :dsl_executor
-        remove_method :dsl_executor
-        meth
-      end
-
-      closure ||= b.binding
-
-      with_closure_from closure do
-        executor.bind(self).call(*args)
-      end
-    end
-
-    private
-
-    def with_closure_from binding, &block
-      @_parent_binding = binding
-      result = block.call
-      @_parent_binding = nil
-      result
-    end
-
-    def method_missing method, *args, &block
-      if @_parent_binding
-        @_parent_binding.send method, *args, &block
-      end
-    end
-  end
-
-  # class NodeRegistry
-  #   class << self
-  #     # @!attribute [rw] registry
-  #     #   @return [Hash<#to_sym, FlowGraph::Node>] Underlying storage of the registry
-  #     def registry
-  #       @@_registry ||= {}
-  #     end
-
-  #     # @param [#to_sym] as_type Name to register the node under
-  #     # @param [FlowGraph::Node] node Class to register as the node
-  #     # @raise [ArgumentError] If +node+ if not of type FlowGraph::Node
-  #     def register_node node, as_type:
-  #       unless node.class == Class && node.ancestors.include?(FlowGraph::Node)
-  #         fail ArgumentError, "Need to register a Node that inherits from FlowGraph::Node!"
-  #       end
-
-  #       registry[ as_type.to_sym ] = node
-  #     end
-
-  #     def unregister_node name
-  #       registry.delete name.to_sym
-  #     end
-
-  #     def get_node name
-  #       registry[ name.to_sym ]
-  #     end
-  #   end
-  # end
 end
